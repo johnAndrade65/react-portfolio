0a0e2d69bbe1f98e83d55bb8fcd67a3559d0b8cb
john65 <joaovini2667@gmail.com>
2022-09-30 09:17:13 -0300

    alpha 0.0.1

---
+++ components/AboutContainer.jsx
@@ -0,0 +1,13 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Olá, me chamo Vinicius Andrade tenho 19 anos e atualmente trabalho como desenvolvedor Front-end!
+        Abaixo você pode conferir algumas das tecnologias que eu domino! e alguns dos meus projetos!.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ components/ProjectsContainer.jsx
@@ -0,0 +1,15 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Quer conferir alguns dos meus projetos? link abaixo!
+      </p>
+      <a href="https://github.com/johnAndrade65?tab=repositories" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
